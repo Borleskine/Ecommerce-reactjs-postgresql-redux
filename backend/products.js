47a2836af9839466d1841c79406eb2ab06ac958b
Bastardo.code <borleskine@hotmail.com>
2023-03-23 14:28:17 -0300

    Actualizar

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 pro",
+        desc: "32mpx 256gb 8ram",
+        price: 899,
+        image: "http://res.cloudinary.com/dcz0x7odq/image/upload/c_scale,w_300/v1679441919/Ecommerce-Sustantiva/iphone12pro_vu0kfj.png",
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "24mpx 256gb 8ram",
+        price: 699,
+        image: "http://res.cloudinary.com/dcz0x7odq/image/upload/c_scale,w_300/v1679441919/Ecommerce-Sustantiva/iphone11_auklsg.jpg",
+    },
+    {
+        id: 3,
+        name: "iPhone 11",
+        desc: "24mpx 128gb 4ram",
+        price: 499,
+        image: "http://res.cloudinary.com/dcz0x7odq/image/upload/c_scale,w_300/v1679441919/Ecommerce-Sustantiva/iphone12_d2azha.jpg",
+    }
+]
+
+module.exports = products;
